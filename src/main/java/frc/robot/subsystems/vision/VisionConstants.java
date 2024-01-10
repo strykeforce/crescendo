01ebb6fc09a67c7dc7e9509d392d4bf775269afc
kennyseybold <42521401+kennyseybold@users.noreply.github.com>
2024-01-09 19:03:34 -0500

    started vision skeleton

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems.vision;
 
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
+
 public class VisionConstants {
-    public static final int kNumCams = 4;
+  public static final int kNumCams = 4;
+  public static final long kMaxTimeNoVision = 5 * 1000000;
+  public static final int kResultsForWheels = 5;
+  public static final long kTimeToDecayDev = 3 * 1000000;
+  public static final double kStdDevDecayCoeff = -0.1;
+  public static final double kMinStdDev = 0.01;
+
+  // Increase these numbers to trust global measurements from vision less. This matrix is in the
+  // form [x, y, theta]ᵀ, with units in meters and radians.
+  // Vision Odometry Standard devs
+  public static Matrix<N3, N1> kVisionMeasurementStdDevs =
+      VecBuilder.fill(0.05, 0.05, Units.degreesToRadians(5000));
 }
--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -1,78 +1,145 @@
 package frc.robot.subsystems.vision;
 
-import java.util.ArrayList;
-
 import WallEye.WallEyeCam;
 import WallEye.WallEyeResult;
+import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.ArrayList;
 
 public class VisionSubsystem extends SubsystemBase {
-    
-    //Private Variables
-    WallEyeCam[] cams;
-    Translation3d[] offsets;
-    String[] names = {"1", "2", "3", "4"};
-    int[] camIndex = {1,2,3,4};
-    ArrayList<Pair<WallEyeResult,Integer>> validResults = new ArrayList<>();
-    VisionStates curState = VisionStates.TRUSTWHEELS;
-    boolean visionUpdates = true;
 
-    //Constructor
-    public VisionSubsystem() {
-        cams = new WallEyeCam[VisionConstants.kNumCams]; 
-        for (int i = 0; i < VisionConstants.kNumCams; ++i) {
-            cams[i] = new WallEyeCam(names[i], camIndex[i], -1);
-        }  
+  // Private Variables
+  WallEyeCam[] cams;
+  Translation3d[] offsets;
+  String[] names = {"1", "2", "3", "4"};
+  int[] camIndex = {1, 2, 3, 4};
+  ArrayList<Pair<WallEyeResult, Integer>> validResults = new ArrayList<>();
+  VisionStates curState = VisionStates.TRUSTWHEELS;
+  boolean visionUpdates = true;
+  long timeLastVision = 0;
+  int updatesToWheels = 0;
 
+  public Matrix<N3, N1> adaptiveVisionMatrix;
+
+  // Constructor
+  public VisionSubsystem() {
+    cams = new WallEyeCam[VisionConstants.kNumCams];
+    adaptiveVisionMatrix = VisionConstants.kVisionMeasurementStdDevs.copy();
+
+    for (int i = 0; i < VisionConstants.kNumCams; ++i) {
+      cams[i] = new WallEyeCam(names[i], camIndex[i], -1);
+    }
+  }
+
+  // Getter/Setter Methods
+  public void setVisionUpdates(boolean visionUpdates) {
+    this.visionUpdates = visionUpdates;
+  }
+
+  public boolean isVisionUpdatingDrive() {
+    return visionUpdates;
+  }
+
+  public boolean isCameraConnected(int index) {
+    return cams[index].isCameraConnected();
+  }
+
+  public VisionStates getState() {
+    return curState;
+  }
+
+  public void setState(VisionStates state) {
+    curState = state;
+  }
+
+  // Helper Methods
+
+  private boolean isPoseValidWithWheels(Translation3d temp) {
+    return true;
+  }
+
+  private boolean isPoseValidWithoutWheels(Translation3d temp) {
+    return true;
+  }
+
+  // Periodic
+  @Override
+  public void periodic() {
+    if (RobotController.getFPGATime() > VisionConstants.kMaxTimeNoVision) {
+      curState = VisionStates.TRUSTVISION;
+      adaptiveVisionMatrix = VisionConstants.kVisionMeasurementStdDevs.copy();
+      updatesToWheels = 0;
     }
 
-    //Getter/Setter Methods
-    public void setVisionUpdates(boolean visionUpdates) {
-        this.visionUpdates = visionUpdates;
+    if (updatesToWheels >= VisionConstants.kResultsForWheels) {
+      curState = VisionStates.TRUSTWHEELS;
     }
 
-    public boolean isVisionUpdatingDrive() {
-        return visionUpdates;
+    validResults.clear();
+    for (int i = 0; i < VisionConstants.kNumCams; ++i) {
+      if (cams[i].hasNewUpdate()) {
+        timeLastVision = RobotController.getFPGATime();
+        validResults.add(new Pair<WallEyeResult, Integer>(cams[i].getResults(), i));
+      }
     }
 
-    public boolean isCameraConnected(int index) {
-        return true;
+    if (validResults.isEmpty()
+        && RobotController.getFPGATime() - timeLastVision >= VisionConstants.kTimeToDecayDev) {
+      for (int i = 0; i < 2; ++i) {
+        double estimatedWeight =
+            VisionConstants.kVisionMeasurementStdDevs.get(i, 0)
+                - VisionConstants.kStdDevDecayCoeff
+                    * (RobotController.getFPGARevision()
+                        - timeLastVision
+                        - VisionConstants.kTimeToDecayDev)
+                    / 1000000;
+        adaptiveVisionMatrix.set(
+            i,
+            0,
+            estimatedWeight > VisionConstants.kMinStdDev
+                ? estimatedWeight
+                : VisionConstants.kMinStdDev);
+      }
     }
 
-    //Helper Methods
+    for (Pair<WallEyeResult, Integer> res : validResults) {
+      Pose3d cameraPose = res.getFirst().getCameraPose();
+      Translation3d centerPose =
+          cameraPose
+              .getTranslation()
+              .plus(offsets[res.getSecond()].rotateBy(cameraPose.getRotation()));
 
-
-    //Periodic
-    @Override
-    public void periodic() {
-        validResults.clear();
-        for (int i = 0; i < VisionConstants.kNumCams; ++i) {
-            if(cams[i].hasNewUpdate()) {
-                validResults.add(new Pair<WallEyeResult,Integer>(cams[i].getResults(), i));
-            }
-        }
-        if (visionUpdates)
-        {
+      if (visionUpdates) {
         switch (curState) {
-            case TRUSTVISION:
-            
+          case TRUSTWHEELS:
+            if (isPoseValidWithWheels(centerPose)) {
+              // Feed into odometry
+
+            }
             break;
-            case TRUSTWHEELS:
-            
+          case TRUSTVISION:
+            if (isPoseValidWithoutWheels(centerPose)) {
+              updatesToWheels++;
+              // Feed into odometry
+            }
+
             break;
         }
+      }
     }
-    }
+  }
 
-    //Grapher
+  // Grapher
 
-
-    //State Enum
-    public enum VisionStates {
-        TRUSTWHEELS,
-        TRUSTVISION
-    }
+  // State Enum
+  public enum VisionStates {
+    TRUSTWHEELS,
+    TRUSTVISION
+  }
 }
