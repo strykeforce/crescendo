b8d3ce6dc15cd7ec904b64f24a37807aa8a8e90d
kennyseybold <42521401+kennyseybold@users.noreply.github.com>
2024-01-11 20:37:51 -0500

    Io

---
+++ src/main/java/frc/robot/subsystems/magazine/MagazineIO.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.magazine;
+
+public interface MagazineIO {
+    public static class MagazineIOInputs {
+        public double velocity;
+        public double position; 
+        public double setpoint;
+    }
+}
