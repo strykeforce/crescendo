ca1dd1124df2c85fc48ba8dcc2cb46c711d3d37e
mwitcpalek <mwitcpalek@gmail.com>
2024-01-13 18:23:25 -0500

    update CTRE and thirdcoast vendordeps, add advantage kit, update examples

---
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot.constants;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "crescendo";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "429d5a41ebfdf286d3fad5cefb4e9ef45fc43117";
+  public static final String GIT_DATE = "2024-01-09 07:18:57 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-01-13 18:22:23 EST";
+  public static final long BUILD_UNIX_TIME = 1705188143327L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
