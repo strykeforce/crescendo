a6f8abfbc6ea0c1bcbcab96ffd096cdd002dfa83
kennyseybold <42521401+kennyseybold@users.noreply.github.com>
2024-01-17 19:34:03 -0500

    added left and right shooter control

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -4,7 +4,8 @@ import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
 public class ShooterConstants {
-  public static final int kShooterTalonID = -1;
+  public static final int kLeftShooterTalonID = -1;
+  public static final int kRightShooterTalonID = -1;
   public static final double kCloseEnough = 100;
 
   public static final TalonFXConfiguration getShooterConfig() {
