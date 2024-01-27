721c9ec606114d607d8e0a488ef0d39fa3718b8f
IRoot <imroot07@gmail.com>
2024-01-26 19:38:18 -0500

    middle note positions, M1 to M2 path

--- src/main/java/frc/robot/constants/AutonConstants.java
+++ src/main/java/frc/robot/constants/AutonConstants.java
@@ -14,11 +14,11 @@ public final class AutonConstants {
     public static final Pose2d W3 = new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
 
     // Middle Notes
-    public static final Pose2d M1 = new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
-    public static final Pose2d M2 = new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
-    public static final Pose2d M3 = new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
-    public static final Pose2d M4 = new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
-    public static final Pose2d M5 = new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
+    public static final Pose2d M1 = new Pose2d(8.270494, 0.752856, Rotation2d.fromDegrees(0.0));
+    public static final Pose2d M2 = new Pose2d(8.270494, 2.429256, Rotation2d.fromDegrees(0.0));
+    public static final Pose2d M3 = new Pose2d(8.270494, 4.105275, Rotation2d.fromDegrees(0.0));
+    public static final Pose2d M4 = new Pose2d(8.270494, 5.781294, Rotation2d.fromDegrees(0.0));
+    public static final Pose2d M5 = new Pose2d(8.270494, 7.457694, Rotation2d.fromDegrees(0.0));
 
     // Shooting Positions
     public static final Pose2d AS1 = new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
