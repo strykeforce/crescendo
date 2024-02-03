ccb54baed444ab3b2a477524c13a979a24c62918
IRoot <imroot07@gmail.com>
2024-02-02 20:13:50 -0500

    open loop wrist command

---
+++ src/main/java/frc/robot/commands/wrist/OpenLoopWristCommand.java
@@ -0,0 +1,21 @@
+package frc.robot.commands.wrist;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.wrist.WristSubsystem;
+
+public class OpenLoopWristCommand extends Command {
+    private final WristSubsystem wristSubsystem;
+    private double percentOutput;
+
+    public OpenLoopWristCommand(WristSubsystem wristSubsystem, double percentOutput) {
+        addRequirements(wristSubsystem);
+
+        this.wristSubsystem = wristSubsystem;
+        this.percentOutput = percentOutput;
+    }
+
+    @Override
+    public void initialize() {
+        wristSubsystem.setPct(percentOutput);
+    }
+}
