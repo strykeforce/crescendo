efc1a63e7a2a0e6dd25dd252b1b5400e98192732
PotatoBoyH4 <isaac.hoekstra@gmail.com>
2024-02-09 19:31:32 -0500

    added intake turning on to the tuning command

--- src/main/java/frc/robot/commands/robotState/TuningShootCommand.java
+++ src/main/java/frc/robot/commands/robotState/TuningShootCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands.robotState;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
+import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.magazine.MagazineSubsystem;
 import frc.robot.subsystems.robotState.RobotStateSubsystem;
 import frc.robot.subsystems.robotState.RobotStateSubsystem.RobotStates;
@@ -13,23 +14,27 @@ public class TuningShootCommand extends Command {
   SuperStructure superStructure;
   MagazineSubsystem magazineSubsystem;
   RobotContainer robotContainer;
+  IntakeSubsystem intakeSubsystem;
 
   public TuningShootCommand(
       RobotStateSubsystem robotStateSubsystem,
       SuperStructure superStructure,
       MagazineSubsystem magazineSubsystem,
-      RobotContainer robotContainer) {
+      RobotContainer robotContainer,
+      IntakeSubsystem intakeSubsystem) {
     addRequirements(superStructure, magazineSubsystem);
     this.robotStateSubsystem = robotStateSubsystem;
     this.superStructure = superStructure;
     this.magazineSubsystem = magazineSubsystem;
     this.robotContainer = robotContainer;
+    this.intakeSubsystem = intakeSubsystem;
   }
 
   @Override
   public void initialize() {
     magazineSubsystem.enableLimitSwitches(false);
     magazineSubsystem.setSpeed(robotContainer.magazineSpeed.getDouble(0.0));
+    intakeSubsystem.toIntaking();
     superStructure.shoot(
         robotContainer.lShooterSpeed.getDouble(0.0),
         robotContainer.duplicateShooters.getBoolean(true)
