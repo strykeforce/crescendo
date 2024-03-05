b45a038027d3f05acf6a847052c2da75c73e7192
Paul Hoekstra <paul.hoekstra@gmail.com>
2024-03-04 20:20:29 -0500

    revert what was broken by the 'merge' attempt

--- src/main/java/frc/robot/commands/auton/AmpInitial_WingNotes_BCommand.java
+++ src/main/java/frc/robot/commands/auton/AmpInitial_WingNotes_BCommand.java
@@ -1,12 +1,21 @@
 package frc.robot.commands.auton;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.drive.DriveAutonCommand;
 import frc.robot.commands.drive.ResetGyroCommand;
+import frc.robot.commands.drive.TurnToAngleCommand;
 import frc.robot.commands.drive.setAngleOffsetCommand;
+import frc.robot.commands.robotState.DistanceShootCommand;
+import frc.robot.commands.robotState.VisionShootCommand;
+import frc.robot.constants.AutonConstants;
 import frc.robot.subsystems.drive.DriveSubsystem;
+import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.magazine.MagazineSubsystem;
 import frc.robot.subsystems.robotState.RobotStateSubsystem;
+import frc.robot.subsystems.superStructure.SuperStructure;
 
 public class AmpInitial_WingNotes_BCommand extends SequentialCommandGroup {
 
@@ -14,7 +23,6 @@ public class AmpInitial_WingNotes_BCommand extends SequentialCommandGroup {
   DriveAutonCommand secondPath;
   DriveAutonCommand thirdPath;
   DriveAutonCommand fourthPath;
-  DriveAutonCommand fifthPath;
   DriveAutonCommand fallbackPath;
   DriveAutonCommand fallbackPath2;
   private boolean hasGenerated = false;
@@ -24,20 +32,42 @@ public class AmpInitial_WingNotes_BCommand extends SequentialCommandGroup {
   public AmpInitial_WingNotes_BCommand(
       DriveSubsystem driveSubsystem,
       RobotStateSubsystem robotStateSubsystem,
+      SuperStructure superStructure,
+      MagazineSubsystem magazineSubsystem,
+      IntakeSubsystem intakeSubsystem,
       String pathOne,
       String pathTwo,
       String pathThree) {
     firstPath = new DriveAutonCommand(driveSubsystem, pathOne, true, true);
-    secondPath = new DriveAutonCommand(driveSubsystem, pathTwo, false, false);
-    thirdPath = new DriveAutonCommand(driveSubsystem, pathThree, false, false);
+    secondPath = new DriveAutonCommand(driveSubsystem, pathTwo, true, false);
+    thirdPath = new DriveAutonCommand(driveSubsystem, pathThree, true, false);
     this.robotStateSubsystem = robotStateSubsystem;
 
     addCommands(
         new ResetGyroCommand(driveSubsystem),
-        new setAngleOffsetCommand(driveSubsystem, 60.0),
-        firstPath);
-    // secondPath,
-    // thirdPath);
+        new setAngleOffsetCommand(driveSubsystem, 50.0),
+        new DistanceShootCommand(
+            robotStateSubsystem,
+            superStructure,
+            magazineSubsystem,
+            intakeSubsystem,
+            AutonConstants.kAI1ToSpeakerDist),
+        firstPath,
+        new WaitCommand(0.1),
+        new AutoWaitNoteStagedCommand(robotStateSubsystem),
+        new VisionShootCommand(
+            robotStateSubsystem, superStructure, magazineSubsystem, intakeSubsystem),
+        new TurnToAngleCommand(driveSubsystem, Rotation2d.fromDegrees(-90.0)),
+        secondPath,
+        new WaitCommand(0.1),
+        new AutoWaitNoteStagedCommand(robotStateSubsystem),
+        new VisionShootCommand(
+            robotStateSubsystem, superStructure, magazineSubsystem, intakeSubsystem),
+        thirdPath,
+        new WaitCommand(0.2),
+        new AutoWaitNoteStagedCommand(robotStateSubsystem),
+        new VisionShootCommand(
+            robotStateSubsystem, superStructure, magazineSubsystem, intakeSubsystem));
   }
 
   public void generateTrajectory() {
