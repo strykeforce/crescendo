402919bd79091478e5c93aa8a5b55a3abac0f497
IRoot <imroot07@gmail.com>
2024-03-12 19:44:39 -0400

    use max lookup constant

--- src/main/java/frc/robot/constants/RobotStateConstants.java
+++ src/main/java/frc/robot/constants/RobotStateConstants.java
@@ -21,6 +21,4 @@ public final class RobotStateConstants {
   public static final double kMaxWristToMoveTrapBar = 0;
   public static final double kTrapTimer = 1.5;
   public static final double kClimbTrapTimer = 1.5; // 0.5
-
-  public static final double kSpinUpDistThreshold = 5.0;
 }
