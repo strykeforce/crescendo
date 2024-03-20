be71710ce68ef16a8da09c454b6e87ec2d6a9bd1
mwitcpalek <mwitcpalek@gmail.com>
2024-03-19 23:19:07 -0400

    slight timing tweaks, remove discretization

--- src/main/java/frc/robot/subsystems/superStructure/SuperStructure.java
+++ src/main/java/frc/robot/subsystems/superStructure/SuperStructure.java
@@ -267,8 +267,13 @@ public class SuperStructure extends MeasurableSubsystem {
     if (!isAuto) {
       shooterSubsystem.setSpeed(leftShooterSpeed);
     }
+
     wristSubsystem.setPosition(wristSetpoint);
-    elbowSubsystem.setPosition(SuperStructureConstants.kElbowMinToMoveWrist);
+    if(elbowSubsystem.getPosition() >= SuperStructureConstants.kElbowMinToMoveWrist) {
+      elbowSubsystem.setPosition(elbowSetpoint);
+    } else {
+      elbowSubsystem.setPosition(SuperStructureConstants.kElbowMinToMoveWrist);
+    }
 
     logger.info("{} -> TRANSFER(STOW)", curState);
     isPrecise = false;
