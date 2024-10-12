b058feb3823b8f0685b78dd4f8db5b11656c0583
mwitcpalek <mwitcpalek@gmail.com>
2024-10-12 17:33:03 -0400

    day 2

--- src/main/java/frc/robot/constants/AutonConstants.java
+++ src/main/java/frc/robot/constants/AutonConstants.java
@@ -199,7 +199,7 @@ public final class AutonConstants {
     public static final Pose2d AS2 = new Pose2d(4.3, 6.5, Rotation2d.fromDegrees(0.0));
     public static final Pose2d AS3 = new Pose2d(1.43764, 7.33425, Rotation2d.fromDegrees(90.0));
     public static final Pose2d MS1 = new Pose2d(4.3, 5.55, Rotation2d.fromDegrees(0.0));
-    public static final Pose2d MS2 = new Pose2d(4.3, 6.5, Rotation2d.fromDegrees(0.0));
+    public static final Pose2d MS2 = new Pose2d(4.0, 6.5, Rotation2d.fromDegrees(0.0)); // (4.3,6.5)
     public static final Pose2d MS3 =
         MI1; // new Pose2d(1.57, 4.737, Rotation2d.fromDegrees(0.0)); // y = 5.05
     public static final Pose2d NAS1 = new Pose2d(4.2, 2.8, Rotation2d.fromDegrees(-33.2));
